70f671d573bd110969ee47992b24ff3137dcdff3
FelipeSieiro <felipedossantos0804@gmail.com>
2025-05-23 16:20:37 -0300

    feat: adiciona a pagina home e refatora header

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Timeline } from 'primeng/timeline';
+import { ButtonModule } from 'primeng/button';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
